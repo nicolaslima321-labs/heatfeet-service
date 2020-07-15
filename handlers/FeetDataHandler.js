f125870f668b4ec1366895e0d00cf53767f12e0d
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-07-14 22:04:21 -0300

    Delete FeetDataHandler.js

--- handlers/FeetDataHandler.js
+++
@@ -1,8 +0,0 @@
-let arduino = require('./ArduinoHandler')
-
-console.log("Handling with feetSensors")
-
-
-exports.modules = arduino.on('analogChange', function(e) {
-  console.log("pin" + e.pin + " : " + e.old_value + " -> " + e.value)
-})
